74ba0a776db94a5b81f628c505233e1cd61afb86
Daniel <subiabrewd@gmail.com>
2022-11-10 23:26:36 +0100

    Update dependency injections

--- src/State/UserSessionStateProcessor.php
+++ src/State/UserSessionStateProcessor.php
@@ -4,25 +4,20 @@ namespace App\State;
 
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProcessorInterface;
-use App\Service\Session\SessionService;
+use App\Service\AuthenticationService;
 use Doctrine\ORM\EntityManagerInterface;
 
 class UserSessionStateProcessor implements ProcessorInterface
 {
-    private SessionService $sessionService;
-    private EntityManagerInterface $entityManager;
-
     public function __construct(
-        SessionService $sessionService,
-        EntityManagerInterface $entityManagerInterface
+        private AuthenticationService $authenticationService,
+        private EntityManagerInterface $entityManagerInterface
     ) {
-        $this->sessionService = $sessionService;
-        $this->entityManager = $entityManagerInterface;
     }
 
     public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): void
     {
-        $this->sessionService->invalidateSession($data->getSessionId());
+        $this->authenticationService->invalidateSession($data->getSessionId());
 
         $this->entityManager->remove($data);
         $this->entityManager->flush();
--- src/State/UserStateProcessor.php
+++ src/State/UserStateProcessor.php
@@ -4,21 +4,15 @@ namespace App\State;
 
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProcessorInterface;
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserStateProcessor implements ProcessorInterface
 {
-    private EntityManager $entityManager;
-    private UserPasswordHasherInterface $userPasswordHasher;
-
     public function __construct(
-        EntityManagerInterface $entityManagerInterface,
-        UserPasswordHasherInterface $userPasswordHasherInterface
+        private EntityManagerInterface $entityManagerInterface,
+        private UserPasswordHasherInterface $userPasswordHasherInterface
     ) {
-        $this->entityManager = $entityManagerInterface;
-        $this->userPasswordHasher = $userPasswordHasherInterface;
     }
     
     public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): void
