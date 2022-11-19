5814fbc89c2bacabd0ddea363f5764e264e4eca8
Daniel <subiabrewd@gmail.com>
2022-11-19 01:20:44 +0100

    Add PhotoLocation resource

---
+++ src/Entity/PhotoLocation.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata as API;
+use App\Repository\PhotoLocationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PhotoLocationRepository::class)]
+#[API\ApiResource(
+    uriTemplate: '/photos/locations',
+    operations: [
+        new API\GetCollection(),
+        new API\Post()
+    ]
+)]
+#[API\ApiResource(
+    uriTemplate: '/photos/locations/{id}',
+    uriVariables: [
+        'id' => new API\Link(
+            fromClass: PhotoLocation::class
+        )
+    ],
+    operations: [
+        new API\Get(),
+        new API\Put(),
+        new API\Delete(),
+        new API\Patch()
+    ]
+)]
+class PhotoLocation
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
---
+++ src/Repository/PhotoLocationRepository.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\PhotoLocation;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<PhotoLocation>
+ *
+ * @method PhotoLocation|null find($id, $lockMode = null, $lockVersion = null)
+ * @method PhotoLocation|null findOneBy(array $criteria, array $orderBy = null)
+ * @method PhotoLocation[]    findAll()
+ * @method PhotoLocation[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class PhotoLocationRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, PhotoLocation::class);
+    }
+
+    public function save(PhotoLocation $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(PhotoLocation $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+//    /**
+//     * @return PhotoLocation[] Returns an array of PhotoLocation objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('p')
+//            ->andWhere('p.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('p.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?PhotoLocation
+//    {
+//        return $this->createQueryBuilder('p')
+//            ->andWhere('p.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
